7d2528c715c7f4a49c2b5d9dcbc4beec21fadcfa
R-PG22 <tsukisigure1225@gmail.com>
2026-05-28 15:17:40 +0900

    initialize

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <mbed.h>
+
+CAN can(PB_12, PB_13, 1e6);
+CANMessage msg;
+constexpr can_id = 4;
+// BufferedSerial pc(USBTX, USBRX, 115200);
+
+int16_t pwm[4] = {0};
+
+int main(){
+    pwm[0] = -10000;
+
+    can.read(msg);
+    if(msg.data[0] == 1){
+        pwm[0] = 0;
+    }
+
+    CANMessage msg(can_id, (const u_int8_t *)pwm, 8);
+    can.write(msg);
+}
