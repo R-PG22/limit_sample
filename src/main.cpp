43e3ad56afcefaee851e3502b871b9e6ea3b9c82
R-PG22 <tsukisigure1225@gmail.com>
2026-05-28 15:19:34 +0900

    add

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,13 @@ int16_t pwm[4] = {0};
 
 int main(){
     pwm[0] = -10000;
+    while(1){
+        can.read(msg);
+        if(msg.data[0] == 1){
+            pwm[0] = 0;
+        }
 
-    can.read(msg);
-    if(msg.data[0] == 1){
-        pwm[0] = 0;
+        CANMessage msg(can_id, (const u_int8_t *)pwm, 8);
+        can.write(msg);
     }
-
-    CANMessage msg(can_id, (const u_int8_t *)pwm, 8);
-    can.write(msg);
 }
